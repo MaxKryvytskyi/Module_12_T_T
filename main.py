67454a81863d754752483c6e8903d18ccd962467
Max <maxkryvytskyiwork@gmail.com>
2023-07-09 23:44:18 +0300

    save

--- main.py
+++ main.py
@@ -19,31 +19,32 @@ def load_adress_book():
     
 # Відповідає за те як саме почати роботу
 def start_work_bot():
-    input_uzer = input("Download contact book? Y/N ---> ").lower()
-    if input_uzer in "y n":
-        if input_uzer == "y":
-            print("Downloading the contact book...")
-            return load_adress_book()
+    adress_book = AddtextsBook()
+    while True:
+        input_uzer = input("Download contact book? Y/N ---> ").lower()
+        if input_uzer in "y n":
+            if input_uzer == "y":
+                print("Downloading the contact book...")
+                return load_adress_book()
+            elif input_uzer == "n":
+                print("Creates new contact book...")
+                return adress_book 
+        else:
+            print("The command is not recognized")
+            continue
 
 # Обробка помилок.
 def input_error(func):
     def inner(*argsi,**kwargs): 
-        try:
+        # try:
             return func(*argsi,**kwargs)
-        except TypeError:
-            return f"Wrong command"
-        except IndexError:
-            return f"Enter name and phone separated by a space!"
-        except ValueError:
-            return f"Incorrect data"
-        except KeyError:
-            return f"Enter another name."
-        except AttributeError:
-            return f"Enter command."
-        except IncorrectDateFormat:
-            return f"Incorrect date format"
-        except IncorrectPhoneeFormat:
-            return f"Incorrect phone format"
+        # except TypeError: return f"Wrong command"
+        # except IndexError: return f"Enter name and phone separated by a space!"
+        # except ValueError: return f"Incorrect data"
+        # except KeyError: return f"Enter another name."
+        # except AttributeError: return f"Enter command."
+        # except IncorrectDateFormat: return f"Incorrect date format"
+        # except IncorrectPhoneeFormat: return f"Incorrect phone format"
     return inner
 
 # Асистент додає ім'я та номер телефону якщо є до книги контактів.
@@ -72,35 +73,30 @@ def add_birthday(*args: str) -> str:
 @input_error
 def change_phone(*args: str) -> str:
     rec = adress_book.get(args[0].capitalize())
-    if rec:
-        return rec.change_phone(Phone(args[1]), Phone(args[2]))
+    if rec: return rec.change_phone(Phone(args[1]), Phone(args[2]))
     return f"Contact wit name {args[0].capitalize()} doesn`t exist."
 
-# Ассистент за ім'ям знаходить в контактах номер.
+# Ассистент за ім'ям знаходить в контактах номер/номера та виводить його.
 @input_error
 def phone(*args: str) -> str:
     rec = adress_book[args[0].capitalize()]
-    try:
-        return f"Phone number {args[0].capitalize()} : {[phone.value for phone in rec.phones]}"
-    except AttributeError:
-        return f"No phone number added to {args[0].capitalize()} contact yet "
+    return rec.phone_print(args[0].capitalize(), rec.phones)
 
 # Видаляє мобільний телефон
 @input_error
 def remove_phones(*args: str) -> str:
     rec = adress_book[args[0].capitalize()]
-    rec.remove_phone(Phone(args[1]))
-    return f"Phone number {args[0].capitalize()} : {args[1]}\nDeleted"
+    num = rec.remove_phone(Phone(args[1]))
+    if num == "This contact has no phone numbers saved": return num
+    return f"Phone number {args[0].capitalize()} : {num}\nDeleted"
 
 # Показує скільки днів лишилося до дня народження
 @input_error
 def days_to_birthday(*args: str):
     rec = adress_book[args[0].capitalize()]
     time = rec.days_to_birthday() 
-    if not time:
-        return f"Contact {args[0].capitalize()} has no stored date of birth"
-    else:
-        return f"To the bottom of the birth of {args[0].capitalize()} remained {time}"
+    if not time: return f"Contact {args[0].capitalize()} has no stored date of birth"
+    else: return f"To the bottom of the birth of {args[0].capitalize()} remained {time}"
     
 # Повертає сторінки книги контактів з кількістю N контактів на сторінці
 @input_error
@@ -111,27 +107,25 @@ def show_page(*args:str) -> None:
     for _ in range(1000):
         try:
             text = next(c)
+            if text == None: raise StopIteration
         except StopIteration:
-            if n > 1 :
-                return f"No more pages"
-            else:
-                return f"No saved contacts"
+            if n > 1 : return f"No more pages"
+            else: return f"No saved contacts"
         stop = input(f"Page : {n}")
-        if stop.lower() == "stop":
-            return ""
+        if stop.lower() == "stop": return ""
         print(text)
         n += 1
 
 # Знаходить за літерами та цифрами контакти 
 @input_error
 def search(*args:str) -> str:
-    pattern = rf"{args[0].lower()}"
+    if len(args[0]) < 3:
+        return f"Minimum search length is 3"
+    pattern = rf"{re.escape(args[0].lower())}"
     coincidence_list = []
     for k, v in adress_book.data.items():
-        if re.findall(pattern, str(v).lower()):
-            coincidence_list.append(f"{k}")
+        if re.findall(pattern, str(v).lower()): coincidence_list.append(f"{k}")
     return adress_book.search_contacts(coincidence_list)
-        
 
 # Пояснює команди та надає шаблони
 def helper(*_):
@@ -139,11 +133,9 @@ def helper(*_):
     count = 0
 
     for k, v in COMMANDS_LIST.items():
-        if count == 0:
-            output += "{:^90}\n".format(" " + "_"*90 + " ")
-        else:
-            output += "{:^90}\n".format("|" + "_"*90 + "|")
-        output += "|{:^90}|\n".format(f" COMMANDS - {k}")
+        if count == 0: output += "{:^90}\n".format(" " + "_"*90 + " ")
+        else: output += "{:^90}\n".format("|" + "_"*90 + "|")
+        output += "|{:^90}|\n".format(f" COMMANDS {count+1} - {k}")
         output += "{:^90}\n".format("|" + "_"*90 + "|")
         output += "|{:^90}|\n".format(v[0])
         output += "|{:^90}|\n".format(v[1])
@@ -161,7 +153,6 @@ def exit_uzer(*_):
     works_bot = False
     return "Good bye!"
     
-
 # Список команд Help
 COMMANDS_LIST = {
     "add" : ["Команда яка додає в книгу контактів:", "Команда(add) Ім'я(...) Телефон(...) День Народження(...)"], 
@@ -169,12 +160,13 @@ COMMANDS_LIST = {
     "add birthday" : ["Команда яка додає дату дня народження до існуючого списку контактів:", "Команда(add birthday) Ім'я(...) День Народження(...)"],
     "birthday" : ["Команда яка показує скільки залишилося до дня народження існуючого списку контактів:", "Команда(birthday) Ім'я(...)"], 
     "change phone": ["Команда яка замінює в книзі контактів неактуальний телефон:", "Команда(change phone) Ім'я(...) Неактуальний Телефон(...) Актуальний Телефон(...)"], 
-    "close, exit, good bye" : ["Команди які закінчують роботу асистента", "Команда(close або exit вбо good bye)"],
+    "close, exit, good bye" : ["Команди які закінчують роботу асистента", "Команда(close, exit або good bye)"],
     "hello": ["Команда привітання", "Команда(hello)"],
     "help" : ["Команда з прикладами команд", "Команда(help)"], 
     "phone" : ["Команда яка з книги контактів виводить номер телефону", "Команда(phone) Ім'я(...)"], 
     "remove phone" : ["Команда яка з книги контактів видаляє номер телефону", "Команда(remove phone) Ім'я(...) Телефон(...)"],
-    "show page" : ["Команда яка виводить книгу контактів посторінково", "Команда(show page) Контактів на сторінці(...)"]
+    "show page" : ["Команда яка виводить книгу контактів посторінково", "Команда(show page) Контактів на сторінці(...)"],
+    "search" : ["Команда яка виводить результат пошуку за патерном", "Команда(search) Патерн(...)"],
 }
 
 # Список команд.
@@ -190,7 +182,7 @@ COMMANDS = {
     phone : ("phone", ), # Виводить номер телефону за ім'ям *
     remove_phones : ("remove phone", ), # Видаляє телефон *
     show_page : ("show page", ), # Виводить книгу контактів посторінково *
-    search : ("search", ) # Пошук в книги контактів за кількома цифрами номера телефону або літерами імені тощо.
+    search : ("search", ) # Пошук в книги контактів за кількома цифрами номера телефону або літерами імені тощо. *
 }
 
 # Знаходить команду.
@@ -200,17 +192,15 @@ def handler(uzer_input: str):
     for _, args_com in COMMANDS.items():
         for a_com in args_com:
             if uzer_input.lower().startswith(a_com):
-                found_keywords.append(a_com)
+                if uzer_input[:len(a_com)].lower() == a_com: found_keywords.append(a_com)
     comannds = list(filter(lambda x: len(x) == max(len(com) for com in found_keywords), found_keywords))
     if not comannds:
-        return "There is no such command"
+        return "There is no such command", None
+    if not comannds: return "There is no such command"
     for command, kwds in COMMANDS.items():
         for kwd in kwds:
-            if comannds[0].startswith(kwd) and len(comannds[0]) == len(kwd):
-                return command, uzer_input[len(kwd):].strip().split()
-    raise TypeError()
-
-
+            if comannds[0].startswith(kwd) and len(comannds[0]) == len(kwd): return command, uzer_input[len(kwd):].strip().split()
+   
 @input_error
 def main():
     while works_bot:
@@ -227,6 +217,5 @@ def main():
 
 if __name__ == "__main__":
     load_book = start_work_bot()
-    if load_book:
-        adress_book = load_book
+    if load_book: adress_book = load_book
     main()
--- my_class.py
+++ my_class.py
@@ -3,28 +3,199 @@ from collections import UserDict
 from datetime import datetime
 
 
-class AddtextsBook(UserDict):
+class Field:
+    def __init__(self, value=None):
+        self.__value = None
+        self.value = value
+        
+    @property
+    def value(self):
+        return self.__value
     
-    def add_record(self, record):
+    @value.setter
+    def value(self, value:str):
+        if value: self.__value = value
+    
+    def __str__(self):
+        return self.value
+    
+    def __repr__(self) -> str:
+        return str(self)
+    
+    # def __eq__(self, other:object) -> bool:
+    #     return self.value == other.value
+
+
+class Name(Field):
+    pass
+    
+
+class Phone(Field):
+    @property
+    def value(self):
+        return self.__value
+    
+    @value.setter
+    def value(self, value: str) -> None:
+        if value:
+            correct_phone = ""
+            for i in value: 
+                if i in "+0123456789": correct_phone += i
+
+            if len(correct_phone) == 13: self.__value = correct_phone # "+380123456789"
+            elif len(correct_phone) == 12: self.__value = "+" + correct_phone # "380123456789"
+            elif len(correct_phone) == 10: self.__value = "+38" + correct_phone # "0123456789"
+            elif len(correct_phone) == 9: self.__value = "+380" + correct_phone # "123456789"
+            else: raise IncorrectPhoneeFormat
+
+
+class Birthday(Field):
+    @property
+    def value(self):
+        return self.__value
+    
+    @value.setter
+    def value(self, value:str) -> None:
+        today = datetime.now()
+        birthday = datetime.strptime(value, r'%d-%m-%Y')
+    
+        if type(today) == type(birthday): self.__value = birthday 
+        else: raise IncorrectDateFormat
+    
+
+class Record:
+    def __init__(self, name:Name, phone: Phone=None, birthday: Birthday=None) -> None:
+        self.name = name
+        self.phones = []
+        self.birthday = birthday
+        if birthday: self.add_to_birthday(birthday)
+        if phone: self.phones.append(phone)
+
+    # Додає номер 
+    def add_phone(self, phones:Phone) -> str:
+        if len(self.phones) < 9:
+            if phones not in self.phones:
+                self.phones.append(phones)
+                return f"Phone {phones} add to contact {self.name}"
+            return f"The contact {self.name} already has the phone {phones}"
+        else: 
+            return f"The limit of phones is 9"
+        
+    # Видаляє номер 
+    def remove_phone(self, phones:Phone) -> None:
+        if len(self.phones) == 0:
+            return "This contact has no phone numbers saved"
+        for n in self.phones:
+            if n.value == phones.value: 
+                self.phones.remove(n)
+                return phones
+
+    # Заміна номера А на номер Б
+    def change_phone(self, old_phone:Phone, new_phone:Phone) -> str:
+        for phones in self.phones:
+            if str(old_phone) == str(phones):
+                self.remove_phone(old_phone)
+                self.add_phone(new_phone)
+                return f"Phone {old_phone} change to {new_phone} for {self.name} contact "
+        return f"Phone {old_phone} for contact {self.name} doesn`t exist"
+        
+    # Відмаловує телефони певного контакту 
+    def phone_print(self, name: Name, phones: Phone) -> str:
+        result = ""
+        
+        dict_phone = {"Phone 1" : "", "Phone 2" : "", "Phone 3" : "", 
+                      "Phone 4" : "", "Phone 5" : "", "Phone 6" : "", 
+                      "Phone 7" : "", "Phone 8" : "", "Phone 9" : ""}
+
+        for n, v in enumerate(phones):
+            dict_phone[f"Phone {n+1}"] = v
+
+        result += " {:^90}".format(" "*31 + "_"*30 + " "*29) + "\n"
+        result += "{:<31}|{:^30}|{:>30}".format("", f"{name} Phones","") + "\n"
+        result += " {:<90}".format(" "*30 + "|" + "_"*30 + "|" + " "*29) + "\n"
+
+        if len(phones) > 0:
+            result += " {:^91}".format("_"*91) + "\n"
+            result += "|{:^30}|{:^30}|{:^29}|".format("Phone 1", f"Phone 2","Phone 3") + "\n"
+            result += "|{:^30}|{:^30}|{:^29}|".format(str(dict_phone["Phone 1"]), str(dict_phone["Phone 2"]), str(dict_phone["Phone 3"])) + "\n"
+            result += "|{:<30}|{:^30}|{:>28}|".format("_"*30, "_"*30, "_"*29) + "\n"
+
+        if len(phones) > 3:
+            result += " {:^91}".format("_"*91) + "\n"
+            result += "|{:^30}|{:^30}|{:^29}|".format("Phone 4", f"Phone 5", "Phone 6") + "\n"
+            result += "|{:^30}|{:^30}|{:^29}|".format(str(dict_phone["Phone 4"]), str(dict_phone["Phone 5"]), str(dict_phone["Phone 6"])) + "\n"
+            result += "|{:<30}|{:^30}|{:>28}|".format("_"*30, "_"*30, "_"*29) + "\n"
+
+        if len(phones) > 6:
+            result += " {:^91}".format("_"*91) + "\n"
+            result += "|{:^30}|{:^30}|{:^29}|".format("Phone 7", f"Phone 8","Phone 9") + "\n"
+            result += "|{:^30}|{:^30}|{:^29}|".format(str(dict_phone["Phone 7"]), str(dict_phone["Phone 8"]), str(dict_phone["Phone 9"])) + "\n"
+            result += "|{:<30}|{:^30}|{:>28}|".format("_"*30, "_"*30, "_"*29) + "\n"
+
+        if len(phones) != 0: return f"{result}"
+        else: return f"No phone number added to {name} contact yet "
+
+
+    # Додає birthday
+    def add_to_birthday(self, birthday:Birthday) -> None:
+        self.birthday = birthday
+
+    # Виводить залишок до дня народження певної людини 
+    def days_to_birthday(self) -> str|None:
+        try:
+            date_birthday = self.birthday.value
+        except AttributeError:
+            return None
+        current_datetime = datetime.now()
+        new_date = date_birthday.replace(year=current_datetime.year)
+        days_birthday = new_date - current_datetime
+
+        hours = int(days_birthday.seconds // 3600)
+        minutes = int((days_birthday.seconds % 3600) // 60)
+        seconds = int(days_birthday.seconds % 60)
+
+        if days_birthday.days >= 0:
+            return f"{days_birthday.days} days, {hours} hours, {minutes} minutes, {seconds} seconds"
+        
+        else:
+            date = date_birthday.replace(year=current_datetime.year+1)
+            days_birthday = date - current_datetime
+            return f"{days_birthday.days} days, {hours} hours, {minutes} minutes, {seconds} seconds"
+
+    def __str__(self):
+        return "{}{}{}".format(self.name, " " + ", ".join([str(p) for p in self.phones]) if self.phones else "", 
+                               " " + f"{self.birthday.value.strftime('%d-%m-%Y') if self.birthday else ''}")
+
+
+class IncorrectDateFormat(Exception):
+    pass
+
+
+class IncorrectPhoneeFormat(Exception):
+    pass
+
+
+class AddtextsBook(UserDict):
+    # Додає в словник экземпляр классу Record
+    def add_record(self, record: Record) -> str:
         if record.name.value not in self.keys():
             self.data[record.name.value] = record
-            return f"{record}add successful"
+            t = str(record) + " "
+            return f"{t}add successful"
         else:
-            return f"Contact with name {record.name} already exist." \
-                "Try add phone command for add extra phone."
+            return f"Contact with name {record.name} already exist. Try add phone command for add extra phone."
 
-
+    # Ітерується за книгою контактів
     def iterator(self, num:int) -> str:
         result = self.create_page(num)
-        if result == None:
-            return "No saved contacts"
+        if result == None: return "No saved contacts"
         for _, value in result.items():
             yield value
 
+    # Розбиває книгу контактів посторінково 
     def create_page(self, num:int) -> dict|None:
 
-        if len(self.data) == 0:
-            return None
+        if len(self.data) == 0: return None
         result_list = {}
         count = 1
         page = 1
@@ -34,7 +205,7 @@ class AddtextsBook(UserDict):
             value_birthday = "No birthday date"
             name_value = f"{i.name.value} "
             phone_value = f" {[el.value for el in i.phones]}"
-            birthday_value = f"{i.birthday.value.strftime('%Y-%m-%d') if i.birthday else value_birthday}"
+            birthday_value = f"{i.birthday.value.strftime('%d-%m-%Y') if i.birthday else value_birthday}"
             
             new_list = [name_value, phone_value, birthday_value]
             new_list1.append(new_list)
@@ -49,14 +220,13 @@ class AddtextsBook(UserDict):
 
         return result_list
 
-    def create_print_page(self, page:int, contacts:list, flag: bool) -> str:
+    # Записує книгу контактів посторінково для виводу в консоль 
+    def create_print_page(self, page:int, contacts:list, flag:bool) -> str:
         result = ""
         n = 12
         pattern = r"[\[\'\'\"\"\]]"  # видаляємо зайве
-        if page > 9:
-            n = 11
-        elif page > 99:
-            n = 10
+        if page > 9: n = 11
+        elif page > 99: n = 10
         if contacts:
             if flag:
                 x = "Page" 
@@ -87,165 +257,24 @@ class AddtextsBook(UserDict):
                 
                 else:
                     new_i = re.sub(pattern, "", phone_value)
-                    if i == 0:
-                        result += " {:^90}".format("_"*92) + "\n"
-                    else:
-                        result += "{:^90}".format("|" + "_"*30 +"|"+ "_"*30 +"|"+ "_"*30 +"|") + "\n"
+                    if i == 0: result += " {:^90}".format("_"*92) + "\n"
+                    else: result += "{:^90}".format("|" + "_"*30 +"|"+ "_"*30 +"|"+ "_"*30 +"|") + "\n"
                     result += "| {:<29}| {:<29}| {:<29}|".format(f"Name : {name_value}", f"Phone {count} :{new_i}", f"Birthday {birthday_value}") + "\n"
             
             result += "{:^90}".format("|" + "_"*30 +"|"+ "_"*30 +"|"+ "_"*30 +"|") + "\n"
             return result
         return None
 
-    def search_contacts(self, name: list):
+    # Виконує пошук в кнізі контактів за ключовим значенням
+    def search_contacts(self, name:list) -> str:
         dict_contacts = {}
         text = f"Nothing found"
         if name:
             num = 0
             for i in name:
-                birthday = self.data[i].birthday.value.strftime('%Y-%m-%d') if self.data[i].birthday else "No birthday date"
+                birthday = self.data[i].birthday.value.strftime('%d-%m-%Y') if self.data[i].birthday else "No birthday date"
                 dict_contacts[num] = [str(self.data[i].name), f" {self.data[i].phones}", birthday]
                 num += 1
             text = self.create_print_page(len(dict_contacts), dict_contacts, False)
         
         return text
-
-class Field:
-
-    def __init__(self, value=None):
-        self.__value = None
-        self.value = value
-        
-    @property
-    def value(self):
-        return self.__value
-    
-    @value.setter
-    def value(self, value: str):
-        if value:
-            self.__value = value
-    
-    def __str__(self):
-        return self.value
-    
-    def __repr__(self) -> str:
-        return str(self)
-    
-    def __eq__(self, other: object) -> bool:
-        return self.value == other.value
-
-
-class Name(Field):
-    pass
-    
-
-class Phone(Field):
-
-    @property
-    def value(self):
-        return self.__value
-    
-    @value.setter
-    def value(self, value: str):
-        if value:
-            correct_phone = ""
-            for i in value: 
-                if i in "+0123456789":
-                    correct_phone += i
-            if len(correct_phone) == 13:    
-                self.__value = correct_phone # "+380123456789"
-            elif len(correct_phone) == 12: 
-                self.__value = "+" + correct_phone # "380123456789"
-            elif len(correct_phone) == 10: 
-                self.__value = "+38" + correct_phone # "0123456789"
-            elif len(correct_phone) == 9:
-                self.__value = "+380" + correct_phone # "123456789"
-            else:
-                raise IncorrectPhoneeFormat
-
-class Birthday(Field):
-
-    @property
-    def value(self):
-        return self.__value
-    
-    @value.setter
-    def value(self, value: str):
-        today = datetime.now()
-        birthday = datetime.strptime(value, r'%Y-%m-%d')
-    
-        if type(today) == type(birthday):
-            self.__value = birthday 
-        else:
-            raise IncorrectDateFormat
-    
-
-class Record:
-
-    def __init__(self, name: str, phone=None, birthday=None):
-        self.name = name
-        self.phones = []
-        self.birthday = birthday
-        if birthday:
-            self.add_to_birthday(birthday)
-        if phone:
-            self.phones.append(phone)
-
-
-    
-    # Додає номер 
-    def add_phone(self, phones: Phone) -> None:
-        if phones not in self.phones:
-            self.phones.append(phones)
-            return f"Phone {phones} add to contact {self.name}"
-        return f"The contact {self.name} already has the phone {phones}"
-
-    # Видаляє номер 
-    def remove_phone(self, phones: Phone) -> None:
-        for n in self.phones:
-            if n.value == phones.value:
-                self.phones.remove(n)
-
-    # Заміна номера А на номер Б
-    def change_phone(self, old_phone: Phone, new_phone: Phone) -> None:
-        if old_phone in self.phones:
-            self.remove_phone(old_phone)
-            self.add_phone(new_phone)
-            return f"Phone {old_phone} change to {new_phone} for {self.name} contact "
-        return f"Phone {old_phone} for contact {self.name} doesn`t exist"
-
-    # Додає birthday
-    def add_to_birthday(self, birthday: Birthday) -> None:
-        self.birthday = birthday
-
-    # Виводить залишок до дня народження певної людини 
-    def days_to_birthday(self) -> str|None:
-        try:
-            date_birthday = self.birthday.value
-        except AttributeError:
-            return None
-        current_datetime = datetime.now()
-        new_date = date_birthday.replace(year=current_datetime.year)
-        days_birthday = new_date - current_datetime
-
-        hours = int(days_birthday.seconds // 3600)
-        minutes = int((days_birthday.seconds % 3600) // 60)
-        seconds = int(days_birthday.seconds % 60)
-
-        if days_birthday.days >= 0:
-            return f"{days_birthday.days} days, {hours} hours, {minutes} minutes, {seconds} seconds"
-        
-        else:
-            date = date_birthday.replace(year=current_datetime.year+1)
-            days_birthday = date - current_datetime
-            return f"{days_birthday.days} days, {hours} hours, {minutes} minutes, {seconds} seconds"
-
-    def __str__(self):
-        return "{}{}{}".format(self.name, " " + ", ".join([str(p) for p in self.phones]) if self.phones else "", " " + f"{self.birthday.value.strftime('%Y-%m-%d') if self.birthday else ''}")
-
-class IncorrectDateFormat(Exception):
-    pass
-
-
-class IncorrectPhoneeFormat(Exception):
-    pass
